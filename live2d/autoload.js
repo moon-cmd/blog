18bb98aef4d44a303298a64a8777516e5b1bef50
moon-cmd <61397805+moon-cmd@users.noreply.github.com>
2022-11-17 00:00:39 +0800

    live2d

--- live2d/autoload.js
+++ live2d/autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
 // const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
-const live2d_path = "/wp-content/themes/Sakura/live2d/"
+const live2d_path = "/live2d/"
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
 	return new Promise((resolve, reject) => {
@@ -33,7 +33,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "/wp-content/themes/Sakura/live2d/live2d_api"
+			cdnPath: "/live2d/live2d_api"
 		});
 	});
 }
